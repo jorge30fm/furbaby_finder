aee2cc3dea578db67f1492b8fff41141cef5b621
Jorge Felix <jorge30fm@gmail.com>
2022-05-30 21:52:26 -0400

    display api data

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,11 @@ var petGender = $("#petGender");
 var petBreed = $("#petBreed");
 var petCity = $("#petCity")
 var swipeLeft =$("#swipeLeft");
-var swipeRight = $("#swipeRight")
+var swipeRight = $("#swipeRight");
+var petDescription = $("#description");
+var petURL = $("#petURL")
+var count = 0;
+var petData;
 
 var requestURL ='https://cors-anywhere.herokuapp.com/https://api.petfinder.com/v2/animals?type=dog&page=2';
 
@@ -51,9 +55,54 @@ fetch('https://api.petfinder.com/v2/oauth2/token', {
         return response.json();
       })
       .then(function (doggieData) {
+        petData = doggieData;
         console.log('>>> doggieData >>>', doggieData);
-      });
+        console.log(petData)
+        displayInfo()
+git
+      })
   });
+
+  var displayInfo = function() {
+    //add pet Image
+    var imageSrc = petData.animals[count].primary_photo_cropped.medium;
+    var imageEl = $('<img src="'+ imageSrc +'">')
+    petPhoto.html(imageEl)
+
+    //add Pet Name
+    var name = petData.animals[count].name;
+    petName.text(name)
+
+    //add age
+    var age = petData.animals[count].age
+    petAge.text(age)
+    //add gender
+    var gender = petData.animals[count].gender
+    petGender.text(gender)
+
+    //add Breed
+    var checkMixedBreed = petData.animals[count].breeds.mixed;
+    if(checkMixedBreed) {
+      var breed = petData.animals[count].breeds.primary;
+      petBreed.text(breed)
+    } else {
+      var primaryBreed = petData.animals[count].breeds.primary;
+      var secondaryBreed = petData.animals[count].breeds.secondary;
+      petBreed.text("Mixed: " + primaryBreed + 'and ' + secondaryBreed)
+    }
+    // add city
+    var city = petData.animals[count].contact.address.city;
+    var state = petData.animals[count].contact.address.state;
+    petCity.text(city + ', ' + state);
+
+    //add description
+    var description = petData.animals[count].description;
+    petDescription.text(description)
+    // add url link
+    var url = petData.animals[count].url;
+    petURL.attr("href", url)
+  }
+
 
 
 
